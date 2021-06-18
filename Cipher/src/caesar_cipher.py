f58aed9b6e1b4972beb99cb51bbf2a5378d074b4
MrDax0001 <maxfencing@web.de>
2021-06-18 12:08:18 +0200

    All tests passed

--- Cipher/src/caesar_cipher.py
+++ Cipher/src/caesar_cipher.py
@@ -2,20 +2,39 @@
 """ Casar Cipher to encode UTF-8 Code
 
 Created 08.06.2021
-Last Revision 08.06.2021
+Last Revision 09.06.2021
 @author Max Weise
 """
 
 
-class Caesar_Cypher(object):
+class Caesar_Cipher(object):
+    """ Hold functionality to de and encript Caesar cipher
 
-    __ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+        NOTE: Only the letters from A - Z are currently used.
+              There is no error handling if any other char is used.
+    """
 
-    @staticmethod
-    def encript(self, plaintext: str):
+    __alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'   # For easier lookup
+
+    def __decode_cipher(cipher_text: str, known_offset: int) -> str:
         pass
 
     @staticmethod
-    def decript(self, cipher_text: str, offset: int = None):
-        pass
+    def encript(plaintext: str, offset: int) -> str:
+        """ Shift every char in string by a given offset."""
+        ret = ''
+        for char in plaintext:
+            i = (Caesar_Cipher.__alphabet.index(char) + offset) % 26
+            ret += Caesar_Cipher.__alphabet[i]
 
+        return ret
+
+    @staticmethod
+    def decript(cipher_text: str, known_offset: int = None) -> str:
+        """ Shift every char in string by a given offset (opposite to encript)."""
+        ret = ''
+        for char in cipher_text:
+            i = (Caesar_Cipher.__alphabet.index(char) - known_offset) % 26
+            ret += Caesar_Cipher.__alphabet[i]
+
+        return ret
---
+++ Cipher/test/test_caesar_cipher.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+""" Test for the caesar cipher
+
+Created 08.06.2021
+Last Revision 09.06.2021
+@author Max Weise
+"""
+
+import os, unittest, sys
+
+from unittest import TestCase
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from src.caesar_cipher import Caesar_Cipher
+
+
+class Test_Caesar_Cipher(TestCase):
+
+    __TEST_WORD = 'CAESAR'
+    __TEST_WORD_ENCR = 'FDHVDU'     # Plaintext: caesar, Offset: 3
+    __TEST_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    __TEST_ALPHABET_ENCR = 'DEFGHIJKLMNOPQRSTUVWXYZABC'
+
+    def test_encript(self):
+        encoded_text = Caesar_Cipher.encript(self.__TEST_WORD, 3)
+
+        self.assertEqual(self.__TEST_WORD_ENCR, encoded_text)
+
+    def test_encript_alphabet(self):
+        encoded_text = Caesar_Cipher.encript(self.__TEST_ALPHABET, 3)
+
+        self.assertEqual(encoded_text, self.__TEST_ALPHABET_ENCR)
+
+    def test_decript_with_offsett(self):
+        decoded_text = Caesar_Cipher.decript(self.__TEST_WORD_ENCR, 3)
+
+        self.assertEqual(self.__TEST_WORD, decoded_text)
+
+    def test_decript_alphabet(self):
+        decoded_text = Caesar_Cipher.decript(self.__TEST_ALPHABET_ENCR, 3)
+
+        self.assertEqual(self.__TEST_ALPHABET, decoded_text)
+
+
+if __name__ == '__main__':
+    unittest.main()
